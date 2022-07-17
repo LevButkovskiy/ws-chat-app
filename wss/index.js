a5c5ff8e856176c342b8cc1c590b18f9e87eab17
Lev Butkovskiy <levbut@gmail.com>
2022-07-17 18:22:33 +0300

    [wss] - initial commit. socket io

---
+++ wss/index.js
@@ -0,0 +1,17 @@
+const {Server} = require("socket.io")
+
+const io = new Server(8080, {
+	cors: {
+		origin: "*",
+	},
+})
+
+io.on("connection", (socket) => {
+	// send a message to the client
+	socket.emit("hello from server", 1, "2", {3: Buffer.from([4])})
+
+	// receive a message from the client
+	socket.on("hello from client", (...args) => {
+		console.log(args)
+	})
+})
